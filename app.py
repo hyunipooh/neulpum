7132001442782d98926c0535fd76a45cfe4768ad
hyunipooh <hyunipooh@gmail.com>
2025-07-16 17:23:55 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
 import os
-import time
-import pandas as pd
-import requests
-from flask import Flask, render_template, jsonify, abort
+from flask import Flask, render_template
 from dotenv import load_dotenv
 
 # ✅ 환경 변수 로드 (카카오 API 키 저장)
@@ -13,7 +10,7 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 # ✅ 카카오 맵 API 키 가져오기 (없으면 None)
-KAKAO_MAP_API_KEY = os.getenv("KAKAO_MAP_API_KEY")
+KAKAO_MAP_API_KEY = os.getenv("KAKAO_API_KEY")
 
 @app.route("/")
 @app.route("/index")
